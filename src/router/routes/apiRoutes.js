5a37faa59c0aa49a7b196d403045e17ebd8352f9
PortoGUI <guibusiness.2000@outlook.com>
2024-04-29 04:35:23 -0300

    Updating ping

--- src/router/routes/apiRoutes.js
+++ src/router/routes/apiRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const DB = require("../../DB/dbConfig");
 const router = express.Router();
 
 router.get('/ping', (req, res) => {
-  res.send('Api Working...');
+  DB.query('SELECT NOW()', (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({error :'Erro ao tentar conectar-se.'});
+    } else {
+      res.json({message: 'Conexão bem-sucedida', date: result.rows[0].now});
+    }
+  });
 });
 
 module.exports = router;
